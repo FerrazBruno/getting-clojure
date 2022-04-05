bfd562b098d646d0f71891de4c54298de0814d10
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2022-04-05 08:44:36 -0300

    Chapter 2: In the Wild

--- src/getting_clojure/core.clj
+++ src/getting_clojure/core.clj
@@ -67,4 +67,59 @@
 ;; CHAPTER 2
 ;; Vectors and Lists
 
+;; One Thing after Another
+[1 2 3 4]
+[1 "two" 3 "four"]
+[true 3 "four" 5]
+[1 [true 3 "four" 5] 6]
+[0 [1 [true 3 "four" 5] 6] 7]
+
+;; A Toolkit of Functions
+(vector true 3 "four" 5)
+(vector)
+(def novels ["Emma" "Coma" "War and Peace"])
+(count novels)
+(first novels)
+(rest novels)
+(rest (rest novels))
+(rest ["Ready Player One"])
+(rest [])
+(def year-book ["1491" "April 1865" "1984" "2001"])
+(def third-book (first (rest (rest year-book))))
+third-book
+(nth year-book 2)
+(year-book 2)
+
+;; Growing Your Vectors
+(conj novels "Carrie")
+(cons "Carrie" novels)
+
+;; Lists
+'(1 2 3)
+()
+'(1 2 3 "four" 5 "six")
+'(1 2.0 2.999 "four" 5.001 "six")
+'([1 2 ("a" "list" "inside a" "vector")] "inside" "a" "list")
+(list 1 2 3 "four" 5 "six")
+(def poems '("Iliad" "Odyssey" "Now We Are Six"))
+(count poems)
+(first poems)
+(rest poems)
+(nth poems 2)
+
+;; Lists versus Vectors
+(conj poems "Jabberwocky")
+(def vector-poems ["Iliad" "Odyssey" "Now We Are Six"])
+(conj vector-poems "Jabberwocky")
+
+;; Staying Out Of Trouble
+(def novels ["Emma" "Coma" "War and Peace"])
+(conj novels "Jaws")
+(def more-novels (conj novels "Jaws"))
+(def novels '("Emma" "Coma" "War and Peace"))
+(conj novels "Jaws")
+
+;; In the Wild
+
+
 
